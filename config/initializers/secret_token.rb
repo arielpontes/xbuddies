53e8eaa4558cdfbe70a6d1aa657de0a5f34c6729
Ariel Pontes <pontes.ariel@gmail.com>
2012-12-27 19:26:18 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xbuddies::Application.config.secret_token = 'cfc1d4b657a0efcde31ad1d429121cb3f136ec904c634c03e4c303481cfe5a1a55b9ad1509d053896ecbbc114d7341648872f10bca192ea8278e67d3f45868d3'
